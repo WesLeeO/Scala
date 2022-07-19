968f210eb376f22c825671f8adfe2cccc9450594
Wesley Nana Davies <wesleynanadavies@gmail.com>
2022-07-19 12:13:30 +0200

    4 commits

--- math.scala
+++ math.scala
@@ -1,6 +1,6 @@
 package mathematics
 
-object PrimerFactors{
+object PrimeFactors{
  
    def primeFactors(n: Long) : List[Long] = {
        val exists = (2L to math.sqrt(n.toDouble).toLong).find(n % _ == 0)
@@ -9,5 +9,11 @@ object PrimerFactors{
            case Some(factor) => factor :: primeFactors(n / factor)
        }
         
+    }
+}
+
+object gcd{
+    def gcd(a: Integer, b: Integer) : Integer = {
+        if (b == 0) a else gcd(b, a % b)
     }
 }
